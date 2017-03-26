da0c6182f08176a2d97fd91fe16802070e37c689
Kenton Vizdos <kvizdos@gmail.com>
2017-03-26 13:35:21 -0400

    Update movement.js

--- games/lasers/js/movement.js
+++ games/lasers/js/movement.js
@@ -37,6 +37,20 @@ window.onload = function() {
 
 }
 
+var scoreKeeper;
+var score = 0;
+
+function startScore() {
+      console.log("GAME STARTED");
+
+  scoreKeeper = setInterval(function(){
+    score = score + 1;
+    document.getElementById('livescore').innerHTML = score;
+    document.getElementById('livescore-alive').innerHTML = score;
+  }, 1800);
+}
+
+
 var gameMech;
 
 function startGame() {
