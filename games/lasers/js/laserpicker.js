375ddd1362eda0ebdb4d97131bf1e57c30ecd9e9
Kenton Vizdos <kvizdos@gmail.com>
2017-03-26 13:50:13 -0400

    Update laserpicker.js

--- games/lasers/js/laserpicker.js
+++ games/lasers/js/laserpicker.js
@@ -32,7 +32,7 @@ function startScore() {
     score = score + 1;
     document.getElementById('livescore').innerHTML = score;
     document.getElementById('livescore-alive').innerHTML = score;
-  }, 2000);
+  }, 3800);
 }
 
 
