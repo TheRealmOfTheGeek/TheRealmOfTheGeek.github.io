291fed7f6646724a9346fbcae420a46da66cf07a
Kenton Vizdos <kvizdos@gmail.com>
2017-03-26 13:33:59 -0400

    Update score.js

--- games/lasers/js/score.js
+++ games/lasers/js/score.js
@@ -1,12 +1,1 @@
-var scoreKeeper;
-var score = 0;
 
-function startScore() {
-      console.log("GAME STARTED");
-
-  scoreKeeper = setInterval(function(){
-    score = score + 1;
-    document.getElementById('livescore').innerHTML = score;
-    document.getElementById('livescore-alive').innerHTML = score;
-  }, 1000);
-}
