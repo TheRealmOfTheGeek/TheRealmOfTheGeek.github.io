4c9492f23860ea1a731eccde51758cd18261e332
Kenton Vizdos <kvizdos@gmail.com>
2017-04-16 19:35:33 -0400

    Update snake.js

--- games/snake/js/snake.js
+++ games/snake/js/snake.js
@@ -971,10 +971,4 @@ SNAKE.Board = SNAKE.Board || (function() {
 
     }; // end return function
 })();
-function getHighScore () {
-    document.getElementById('high-score').addEventListener('click', function () {
-        if (localStorage.jsSnakeHighScore == undefined) alert('You have not played this game yet!');
-        else
-    alert('Your current high score is ' + localStorage.jsSnakeHighScore + '.'); });
-}
-getHighScore();
+
