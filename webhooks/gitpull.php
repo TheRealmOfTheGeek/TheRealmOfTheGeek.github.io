0c8b01e7d09a42c42deb551d7e721a85671c4fb0
Kenton Vizdos <kvizdos@gmail.com>
2017-04-15 20:52:56 -0400

    Update gitpull.php

--- webhooks/gitpull.php
+++ webhooks/gitpull.php
@@ -1,4 +1,4 @@
 <?php
-shell_exec('/home/ubuntu/web/pull.sh');
+shell_exec('bash /home/ubuntu/web/pull.sh');
 echo "The Server has updated from github successfully! You shouldn't be here if you aren't GitHub Webhooks, so <a href='/'>go to the main page</a>";
 ?>
