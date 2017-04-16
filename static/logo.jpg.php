e0daa57cd22f7bf00fa6b9952559a9ee4455130e
Kenton Vizdos <kvizdos@gmail.com>
2017-04-16 19:26:04 -0400

    Delete logo.jpg.php

--- static/logo.jpg.php
+++
@@ -1,9 +0,0 @@
-<?php
-include '../includes/rotg.php';
-$info = file_get_contents('https://api.nick.tools/youtubechannel?id=UCuX4KZBMpQLToSwJWy-jCBw');
-$info = json_decode($info, true);
-$url = $info['channel']['image'];
-$content = file_get_contents($url);
-header('Content-Type: image/jpeg');
-echo $content;
-?>
