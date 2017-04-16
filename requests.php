d0d3e164ebc53f05f1bf4fe5a296eb7e4ebf9886
Kenton Vizdos <kvizdos@gmail.com>
2017-04-15 21:31:45 -0400

    Update requests.php

--- requests.php
+++ requests.php
@@ -1,7 +1,9 @@
 <pre>
 <?php
 $reqs = shell_exec('cat /home/ubuntu/web/access.log');
-foreach(explode("\n",$reqs) as $r) {
+$d = explode("\n",$reqs);
+echo "Total Requests: " . count($d) . "\n";
+foreach($d as $r) {
   $parts = explode(" ", $r);
   $parts[0] = substr(md5($parts[0]), 0, 8);
   echo implode(" ", $parts) . "\n";
