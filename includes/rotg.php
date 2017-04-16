a79a6987e088e39a64ee80a3036940ac5e2a63a2
Nick <nick-2017@users.noreply.github.com>
2017-04-16 15:19:21 +0100

    Update rotg.php

--- includes/rotg.php
+++ includes/rotg.php
@@ -1,5 +1,9 @@
 <?php
 header('Server: ROTGServer');
+if (isset($_GET['debug'])) {
+	error_reporting(E_ALL);
+	ini_set('display_errors', 1);
+}
 $year = date('Y');
 $email = 'contact@therotg.com';
 $author = 'Kenton Vizdos';
