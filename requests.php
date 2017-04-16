4b4af3bca8cec0aa8b021c69f8fdfc0cb9efebe8
Nick <nick-2017@users.noreply.github.com>
2017-04-16 14:51:28 +0100

    Update requests.php

--- requests.php
+++ requests.php
@@ -1,8 +1,10 @@
 <pre>
 <?php
 $reqs = shell_exec('cat /home/ubuntu/web/access.log');
+$u = shell_exec('cat /home/ubuntu/web/access.log | awk '{print $1}' | sort | uniq | wc -l');
 $top = shell_exec("cat /home/ubuntu/web/access.log | awk '{print $5}' | sort | uniq -c | sort -rh");
 $d = explode("\n",$reqs);
+echo "Unique Visitors: " . $u . "\n";
 echo "Total Requests: " . count($d) . "\n";
 echo "Top Pages:\n" . $top . "\n";
 foreach($d as $r) {
