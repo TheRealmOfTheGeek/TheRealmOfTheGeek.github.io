55343c7a781f9772f00801ce0556f53f92127414
Kenton Vizdos <kvizdos@gmail.com>
2017-04-15 21:06:16 -0400

    Update gitpull.php

--- webhooks/gitpull.php
+++ webhooks/gitpull.php
@@ -1,4 +1,11 @@
 <?php
+$i = json_decode(file_get_contents('https://api.nick.tools/ipinfo?ip=' . $_SERVER['REMOTE_ADDR']), true)['info'];
+$org = $i['org'];
+if (stripos($org, 'GitHub') !== false) {
 shell_exec('bash /home/ubuntu/web/pull.sh');
-echo "The Server has updated from github successfully! You shouldn't be here if you aren't GitHub Webhooks, so <a href='/'>go to the main page</a>";
+echo "The Server has updated from github successfully!";
+} else {
+  http_response_code(403);
+}
 ?>
+
