741754f09ed5b6dc74076f2c76e7cc8784ec76be
Nick <nick-2017@users.noreply.github.com>
2017-04-16 15:18:49 +0100

    Update logo.php

--- static/logo.php
+++ static/logo.php
@@ -1,4 +1,5 @@
 <?php
+include '../includes/rotg.php';
 $info = file_get_contents('https://api.nick.tools/youtubechannel?id=UCuX4KZBMpQLToSwJWy-jCBw');
 $info = json_decode($info, true);
 $url = $info['channel']['image'];
