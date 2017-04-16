31532824b01b111b85680abb46078955ccd7f4e1
Kenton Vizdos <kvizdos@gmail.com>
2017-04-15 21:24:05 -0400

    Update requests.php

--- requests.php
+++ requests.php
@@ -1,7 +1,7 @@
 <pre>
 <?php
 $reqs = shell_exec('cat /home/ubuntu/web/access.log');
-foreach($reqs as $r) {
+foreach(explode("\n",$reqs) as $r) {
   $parts = explode(" ", $r);
   $parts[0] = substr(md5($parts[0]), 0, 8);
   echo implode(" ", $parts) . "\n";
