6613e69b9357b7edeb1c980fc2a2aa79cc569f15
Nick <nick-2017@users.noreply.github.com>
2017-04-17 17:45:24 +0100

    Update rotg.js

--- static/rotg.js
+++ static/rotg.js
@@ -1,6 +1,13 @@
 var loaded = false;
 window.onload = function() {
   console.log('Loaded ROTG JavaScript!');
-  $.get('/testing');
+  var _rollbarConfig = {
+      accessToken: "47930800063d451cb5b6a9f4c1c094af",
+      captureUncaught: true,
+      payload: {
+          environment: "test"
+      }
+  };
+  !function(r){function e(t){if(o[t])return o[t].exports;var n=o[t]={exports:{},id:t,loaded:!1};return r[t
   loaded = true;
 }
