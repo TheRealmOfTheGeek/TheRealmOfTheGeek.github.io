243b703064babff0eb0a13fb50ac4d3eedd820d2
Kenton Vizdos <kvizdos@gmail.com>
2017-04-18 13:57:06 -0400

    Added header module

--- index.php
+++ index.php
@@ -5,53 +5,7 @@ include 'includes/rotg.php';
 <html lang="en">
 <head>
   <title>TheROTG.com</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="static/bootstrap.css">
-  <link rel="stylesheet" href="../style.css">
-  <?php echo $meta; ?>
-  <script src="static/jquery.js"></script>
-  <script src="static/rotg.js"></script>
-  <script src="static/bootstrap.js"></script>
-  <style>
-  * {
-    margin: 0;
-    padding: 0;
-  }
-  #header {
-    height: 100%:
-  }
-
-  </style>
-  <script>
-    $.get('https://www.nick-e.co.uk/tools/api/youtubechannel?source=rotgwebsiteajax&id=UCuX4KZBMpQLToSwJWy-jCBw', function(data) {
-      window.onload = function() {
-      document.getElementById('latestvideotitle').innerHTML = data.newvid.title;
-      document.getElementById('latestvideoframe').src = data.newvid.embedurl;
-      document.getElementById('subs').innerHTML = data.channel.stats.subs;
-      document.getElementById('views').innerHTML = data.channel.stats.views;
-      document.getElementById('videos').innerHTML = data.channel.stats.videos;
-      }
-    });
-  </script>
-    <script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-47286500-3', 'auto');
-  ga('send', 'pageview');
-
-</script>
-  
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<script>
-  (adsbygoogle = window.adsbygoogle || []).push({
-    google_ad_client: "ca-pub-4499070912220787",
-    enable_page_level_ads: true
-  });
-</script>
+  <?php include="modules/header.php"; ?>
 </head>
 <body>
 
---
+++ modules/header.php
@@ -0,0 +1,43 @@
+<?php 
+
+  echo
+  '
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="static/bootstrap.css">
+  <link rel="stylesheet" href="../style.css">
+  <?php echo $meta; ?>
+  <script src="static/jquery.js"></script>
+  <script src="static/rotg.js"></script>
+  <script src="static/bootstrap.js"></script>
+  <style>
+  * {
+    margin: 0;
+    padding: 0;
+  }
+  #header {
+    height: 100%:
+  }
+
+  </style>
+    <script>
+  (function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,"script","https://www.google-analytics.com/analytics.js","ga");
+
+  ga("create", "UA-47286500-3", "auto");
+  ga("send', 'pageview");
+
+  </script>
+    
+  <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+  <script>
+    (adsbygoogle = window.adsbygoogle || []).push({
+      google_ad_client: "ca-pub-4499070912220787",
+      enable_page_level_ads: true
+    });
+  </script>
+  ';
+
+?>
