0b9121bd9b54d7cfe89f0dd51a78c9d1bb7123fc
Nick <nick-2017@users.noreply.github.com>
2017-04-18 14:11:13 +0100

    Update index.php

--- games/snake/index.php
+++ games/snake/index.php
@@ -1,7 +1,7 @@
 
 <html>
     <head>
-    <title>SNAKE | Kenton Vizdos Dot Com</title>
+    <title>SNAKE | TheROTG.com</title>
     <link rel="stylesheet" id="style" type="text/css" href="./css/dark-snake.css">
 	<link rel="icon" href="https://therotg.com/static/logo.jpg" sizes="16x16" type="image/jpeg">
 		<link rel="shortcut icon" href="https://therotg.com/static/logo.jpg" sizes="16x16" type="image/jpeg">
@@ -25,17 +25,7 @@ function getTheme () {
           case 1: changeTheme('css/main-snake.css');
     }
 }
-if (navigator.onLine && window.location.hostname === 'patorjk.com') {
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'UA-3312460-1']);
-    _gaq.push(['_trackPageview']);
 
-    (function() {
-      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    })();
-}
 </script>
 
 
