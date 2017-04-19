74bacd2167b11226297cbf75e44093bbadaf1a0d
Kenton Vizdos <kvizdos@gmail.com>
2017-04-19 14:58:23 -0400

    Create pull.php

---
+++ webhooks/pull.php
@@ -0,0 +1,13 @@
+<?php
+$i = json_decode(file_get_contents('https://api.nick.tools/ipinfo?ip=' . $_SERVER['REMOTE_ADDR']), true)['info'];
+$org = $i['org'];
+if (stripos($org, 'GitHub') !== false) {
+  echo "Starting the pull!!!\n";
+//echo shell_exec('bash /home/ubuntu/web/pull.sh');
+//echo shell_exec('bash /home/ubuntu/web/restart.sh');
+echo "\n The Server has updated from github successfully!";
+} else {
+  http_response_code(403); // return error 403 forbidden if they're not allowed to pull
+}
+?>
+
