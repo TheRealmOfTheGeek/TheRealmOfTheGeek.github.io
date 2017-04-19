591a435a94de68d1fa7a7d5b3d6911e6c5ad9c13
Kenton Vizdos <kvizdos@gmail.com>
2017-04-18 22:32:22 -0400

    Update requests.php

--- requests.php
+++ requests.php
@@ -1,10 +1,10 @@
 <pre>
 <?php
 $exclude = '172.26.8.61 text/html; charset=UTF-8 "-" | "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.2.13) Gecko/20101203 Firefox/3.6.13"';
-$reqs = shell_exec("cat /home/ubuntu/web/access.log | grep -v '" . $exclude . "'");
-$u = shell_exec("cat /home/ubuntu/web/access.log | grep -v '" . $exclude . "' |awk '{print $1}' | sort | uniq | wc -l");
+$reqs = shell_exec("cat /home/ubuntu/web/log/access.log | grep -v '" . $exclude . "'");
+$u = shell_exec("cat /home/ubuntu/web/log/access.log | grep -v '" . $exclude . "' |awk '{print $1}' | sort | uniq | wc -l");
 $u = str_replace("\n","",$u);
-$top = shell_exec("cat /home/ubuntu/web/access.log | grep -v '" . $exclude . "' | awk '{print $5}' | sort | uniq -c | sort -rh");
+$top = shell_exec("cat /home/ubuntu/web/log/access.log | grep -v '" . $exclude . "' | awk '{print $5}' | sort | uniq -c | sort -rh");
 $d = explode("\n",$reqs);
 echo "Unique Visitors: " . $u . "\n";
 echo "Total Requests: " . count($d) . "\n";
