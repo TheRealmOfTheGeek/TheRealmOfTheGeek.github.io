fd5f7fb2dcc56456b2a25531ea5c796a4a6a5466
Kenton Vizdos <kvizdos@gmail.com>
2017-04-23 20:55:54 -0400

    Update index.php

--- social/index.php
+++ social/index.php
@@ -1,24 +1,15 @@
 <html>
 <head>
-  <title>The Realm Of The Geek Social Links</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="/static/bootstrap.css">
-  <link rel="stylesheet" href="style.css">
-
-  <script src="/static/jquery.js"></script>
-  <script src="/static/bootstrap.js"></script>
-   <script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-47286500-3', 'auto');
-  ga('send', 'pageview');
-
-</script>
+	<?php 
+		$page = basename(__FILE__, ".php");
+		include("modules/header.php"); 
+	
+	?>
 </head>
+	
+<body>
+
+<?php include("modules/navbar.php"); ?>
 <body>
   <center><h1>Social Links</h1></center>
   <center><h2><a href="http://s.therotg.com/twitter" target="_blank">Twitter</a> | <a href="http://s.therotg.com/youtube" target="_blank">YouTube</a> | <a href="http://s.therotg.com/trello">Trello</a> | <a href="http://s.therotg.com/github">GitHub</a></h2></center>
