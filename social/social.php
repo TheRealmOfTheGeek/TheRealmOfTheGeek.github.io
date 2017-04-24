f2bfca02697466e5a0f0a0c715a49d90047aa47b
Kenton Vizdos <kvizdos@gmail.com>
2017-04-23 21:06:03 -0400

    Update social.php

--- social/social.php
+++ social/social.php
@@ -14,5 +14,7 @@
   <center><h1>Social Links</h1></center>
   <center><h2><a href="http://s.therotg.com/twitter" target="_blank">Twitter</a> | <a href="http://s.therotg.com/youtube" target="_blank">YouTube</a> | <a href="http://s.therotg.com/trello">Trello</a> | <a href="http://s.therotg.com/github">GitHub</a></h2></center>
   <center><h3>Contact Me: contact@therotg.com<br><a href="/">Go To The Main Website</a>
+<?php include("../modules/footer.php"); ?>
+
 </body>
 </html>
