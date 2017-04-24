3aab736a4de03afbf94f3598e54267ebb2667365
Kenton Vizdos <kvizdos@gmail.com>
2017-04-23 21:05:34 -0400

    Update calendar.php

--- calendar/calendar.php
+++ calendar/calendar.php
@@ -19,9 +19,8 @@
   </div>
 <iframe src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showNav=0&amp;showDate=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=dn0sg1o4icnhntir0k6umoqb0o%40group.calendar.google.com&amp;color=%232F6309&amp;ctz=America%2FNew_York" style="border-width:0" width="100%" height="600" frameborder="0" scrolling="no"></iframe></div>
 
-<footer class="container-fluid text-center">
-  <p>(c) Kenton Vizdos 2017</p>
-</footer>
+<?php include("../modules/footer.php"); ?>
+
 
 </body>
 </html>
