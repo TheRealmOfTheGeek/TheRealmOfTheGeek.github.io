9f49c53cf30b089cf8dee3ff462c902fabf9c579
Kenton Vizdos <kvizdos@gmail.com>
2017-04-23 20:55:01 -0400

    Update index.php

--- calendar/index.php
+++ calendar/index.php
@@ -1,62 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Calendar | TheROTG.com</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="/static/bootstrap.css">
-  <link rel="stylesheet" href="../style.css">
-
-  <script src="/static/jquery.js"></script>
-  <script src="/static/bootstrap.js"></script>
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-47286500-3', 'auto');
-  ga('send', 'pageview');
-
-</script>
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-47286500-3', 'auto');
-  ga('send', 'pageview');
-
-</script>
+	<?php 
+		$page = basename(__FILE__, ".php");
+		include("modules/header.php"); 
+	
+	?>
 </head>
+	
 <body>
 
-<nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header navbar">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-     <a class="navbar-brand" href="/">TheROTG.com</a>
-    </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav">
-        <li><a href="/#about">About</a></li>
-        <li><a href="/#plugins">Projects</a></li>
-        <li><a href="/#gig">Hire Me</a></li>
-        <li><a href="/#contact">Contact</a></li>
-        <li><a href="#">Calendar</a></li>
-        <li><a href="/videos/">Learn To Code</a></li>
-        <li><a href="../games/index.html">Fun Games</a></li>
-
-      </ul>
-
-    </div>
-  </div>
-</nav>
+<?php include("modules/navbar.php"); ?>
 
 <div class="jumbotron" id="header">
   <div class="container text-center">
