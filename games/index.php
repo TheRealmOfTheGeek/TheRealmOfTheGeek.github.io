22c320f4c4030715aae9f0f2b995666f069c30a7
Kenton Vizdos <kvizdos@gmail.com>
2017-04-23 20:55:31 -0400

    Update index.php

--- games/index.php
+++ games/index.php
@@ -1,30 +1,15 @@
 <html>
-  <head>
-    <title>Original Games By Kenton Vizdos</title>
+<head>
+	<?php 
+		$page = basename(__FILE__, ".php");
+		include("modules/header.php"); 
+	
+	?>
+</head>
+	
+<body>
 
-<meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="/static/bootstrap.css">
-  <script src="/static/jquery.js"></script>
-  <script src="/static/bootstrap.js"></script>
-  <style>
-  .indev {
-    color: rgb(1, 202, 223);
-  }
-  .comp {
-    color: rgb(23, 173, 56);
-  }
-  </style>
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-47286500-3', 'auto');
-  ga('send', 'pageview');
-
-</script>
-  </head>
+<?php include("modules/navbar.php"); ?>
 
   <body>
 <div class="container">
