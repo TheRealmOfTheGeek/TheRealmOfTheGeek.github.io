8bd69a15ce57dfe58e1a3bee40c1d4af6eb9f492
Kenton Vizdos <kvizdos@gmail.com>
2017-04-23 20:58:03 -0400

    Update index.php

--- social/index.php
+++ social/index.php
@@ -9,7 +9,7 @@
 	
 <body>
 
-<?php include("modules/navbar.php"); ?>
+<?php include("../modules/navbar.php"); ?>
 <body>
   <center><h1>Social Links</h1></center>
   <center><h2><a href="http://s.therotg.com/twitter" target="_blank">Twitter</a> | <a href="http://s.therotg.com/youtube" target="_blank">YouTube</a> | <a href="http://s.therotg.com/trello">Trello</a> | <a href="http://s.therotg.com/github">GitHub</a></h2></center>
