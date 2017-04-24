3a64e4386414652151a70ba67bcb94a43d0eb5fa
Kenton Vizdos <kvizdos@gmail.com>
2017-04-23 21:02:56 -0400

    Update and rename title.php to footer.php

---
+++ modules/footer.php
@@ -0,0 +1,4 @@
+<footer class="container-fluid text-center">
+	<script src="//z-na.amazon-adsystem.com/widgets/onejs?MarketPlace=US&adInstanceId=b44ee25c-ee86-43e5-aa2e-1d1c19d5ec7e"></script>	<p>(c) Kenton Vizdos - 2017</p>
+	<p>© Kenton Vizdos 2017</p>
+</footer>
--- modules/title.php
+++
@@ -1,5 +0,0 @@
-<?php 
-	$page = basename(__FILE__, '.php');
- if($page == "index") { $page == "home"; }
-  echo "<title>$page | TheROTG</title>";
-?>  
