cf99c47e5cf28670d38261fb0f67521a9f962843
Kenton Vizdos <kvizdos@gmail.com>
2017-04-23 21:30:37 -0400

    Update footer.php

--- modules/footer.php
+++ modules/footer.php
@@ -1,4 +1,3 @@
 <footer class="container-fluid text-center">
-	<script src="//z-na.amazon-adsystem.com/widgets/onejs?MarketPlace=US&adInstanceId=b44ee25c-ee86-43e5-aa2e-1d1c19d5ec7e"></script>
 	<p>© Kenton Vizdos 2017</p>
 </footer>
