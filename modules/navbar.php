c36aaeb973cb94ff3c9b40ffea4b792f1234971f
Kenton Vizdos <kvizdos@gmail.com>
2017-04-23 20:53:34 -0400

    Update navbar.php

---
+++ modules/navbar.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header navbar">
+
+      <a class="navbar-brand" href="../home.php">TheROTG.com</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li><a href="#about">About</a></li>
+        <li><a href="#plugins">Projects</a></li>
+        <li><a href="#gig">Hire Me</a></li>
+        <li><a href="#contact">Contact</a></li>
+        <li><a href="../calendar/index.php">Calendar</a></li>
+        <li><a href="../videos/index.php">Learn To Code</a></li>
+        <li><a href="../games/index.html">Fun Games</a></li>
+
+      </ul>
+    </div>
+  </div>
+</nav>
