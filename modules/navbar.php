4a80c95947f3309963ae2a6b195d848e234576bb
Kenton Vizdos <kvizdos@gmail.com>
2017-04-23 21:02:04 -0400

    Update navbar.php

--- modules/navbar.php
+++ modules/navbar.php
@@ -6,10 +6,10 @@
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav">
-        <li><a href="../index.php#about">About</a></li>
-        <li><a href="../index.php#plugins">Projects</a></li>
-        <li><a href="../index.php#gig">Hire Me</a></li>
-        <li><a href="../index.php#contact">Contact</a></li>
+        <li><a href="../home.php#about">About</a></li>
+        <li><a href="../home.php#plugins">Projects</a></li>
+        <li><a href="../home.php#gig">Hire Me</a></li>
+        <li><a href="../home.php#contact">Contact</a></li>
         <li><a href="../calendar/calendar.php">Calendar</a></li>
         <li><a href="../videos/videos.php">Learn To Code</a></li>
         <li><a href="../games/games.php">Fun Games</a></li>
