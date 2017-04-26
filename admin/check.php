31f352a8b90e7c1d6e07c63ed5a28d0ba0be69e1
Nick <nick-2017@users.noreply.github.com>
2017-04-26 19:05:51 +0100

    Update check.php

--- admin/check.php
+++ admin/check.php
@@ -1,5 +1,6 @@
 <?php
 $i = file_get_contents('https://api.nick.tools/ipinfo?ip=' . $_SERVER['REMOTE_ADDR']);
+$i = json_decode($i,true)['info'];
 $allowed = $i['people']['nick'] || (isset($i['people']['kento']) && $i['people']['kento']);
 if (!$allowed) {
 echo '
