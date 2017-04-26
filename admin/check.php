2291d949b8b7c7494a6ca40573a510f7f9158079
Nick <nick-2017@users.noreply.github.com>
2017-04-26 19:03:14 +0100

    Create check.php

---
+++ admin/check.php
@@ -0,0 +1,21 @@
+<?php
+$i = file_get_contents('https://api.nick.tools/ipinfo?ip=' . $_SERVER['REMOTE_ADDR']);
+$allowed = $i['people']['nick'] || (isset($i['people']['kento']) && $i['people']['kento']);
+if (!$allowed) {
+echo '
+<iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=PL8wKRvqDMhS-yaig0ZUkrNLBx2qvhIbaH&version=3&autoplay=1&loop=1" frameborder="0" allowfullscreen></iframe>
+<--
+';
+
+for ($k = 0; $k < 200; $k++){
+echo '
+';
+}
+echo '
+
+Error 403: Forbidden
+
+-->';
+exit();
+}
+?>
