be808db3695ac56ded55cc76018c6c7f9a6851a9
Kenton Vizdos <kvizdos@gmail.com>
2017-04-28 17:35:55 -0400

    Update header.php

--- forum/modules/header.php
+++ forum/modules/header.php
@@ -1,7 +1,6 @@
 
 <div id="header">
       <h1 id="title" ng-bind="title"></h1>
+      <input type="text" placeholder="Search the forums!" />
       <small class="rightnav"><a href="login.php">Login / Register</a></small>
-
-<gcse:search></gcse:search>
 </div>
