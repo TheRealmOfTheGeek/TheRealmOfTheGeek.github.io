51227b4a4a90898ceea8186a829c922107031cf5
Kenton Vizdos <kvizdos@gmail.com>
2017-04-28 18:02:49 -0400

    Update header.php

--- forum/modules/header.php
+++ forum/modules/header.php
@@ -1,13 +1,12 @@
 <?php
 function showheader() {
      echo '
+     <meta name="google-signin-client_id" content="'.$secret['id'].'.apps.googleusercontent.com">
+     <script src="https://apis.google.com/js/platform.js" async defer></script>
      <div id="header">
-          <meta name="google-signin-client_id" content="'.$secret['id'].'.apps.googleusercontent.com">
-          <script src="https://apis.google.com/js/platform.js" async defer></script>
            <h1 id="title" ng-bind="title"></h1>
           <input type="text" placeholder="Search the forums!" class="rightnav" />
           <div class="g-signin2" data-onsuccess="onSignIn"></div>
-
      </div>
 ';
 }
