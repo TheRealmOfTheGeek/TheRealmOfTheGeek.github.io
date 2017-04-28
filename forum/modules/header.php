83fa6dc505d9d631b95b4afeeee4a6c0580ca9eb
Kenton Vizdos <kvizdos@gmail.com>
2017-04-28 17:27:07 -0400

    Update header.php

--- forum/modules/header.php
+++ forum/modules/header.php
@@ -1,14 +1,4 @@
-<script>
-  (function() {
-    var cx = '003950137451284587692:mwg_k-ywuom';
-    var gcse = document.createElement('script');
-    gcse.type = 'text/javascript';
-    gcse.async = true;
-    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(gcse, s);
-  })();
-</script>
+
 <div id="header">
       <h1 id="title" ng-bind="title"></h1>
       <small class="rightnav"><a href="login.php">Login / Register</a></small>
