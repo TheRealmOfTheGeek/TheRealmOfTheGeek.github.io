708b35be2f46753ab9161a32bbb9ba0346322d28
Kenton Vizdos <kvizdos@gmail.com>
2017-04-28 17:19:53 -0400

    Update header.php

--- forum/modules/header.php
+++ forum/modules/header.php
@@ -1,5 +1,5 @@
 <div id="header">
       <h1 id="title" ng-bind="title"></h1>
       <small class="rightnav"><a href="login.php">Login / Register</a></small>
-      <input type="text" placeholder="Search.." />
+      <input class="rightnav" type="text" placeholder="Search.." />
 </div>
