4aa3b4bb0f9116fe36a443df913c54e045127be8
Nick <nick-2017@users.noreply.github.com>
2017-04-28 23:14:14 +0100

    Update login.php

--- forum/login.php
+++ forum/login.php
@@ -1,1 +1,54 @@
-
+<?php
+include '/home/ubuntu/keys/oauth.php';
+function showsignin() {
+	echo '<div id="my-signin2"></div>
+  <script>
+    function onSuccess(googleUser) {
+      console.log("Logged in as: " + googleUser.getBasicProfile().getName());
+	  getElement("i").value = googleUser.getAuthResponse().id_token;
+	  getElement("s").click();
+    }
+    function onFailure(error) {
+      alert("Error!");
+    }
+	function logout() {
+		var auth2 = gapi.auth2.getAuthInstance();
+		auth2.signOut().then(function () {
+		});
+	}
+    function renderButton() {
+		' . "
+      gapi.signin2.render('my-signin2', {
+        'scope': 'profile email',
+        'width': 240,
+        'height': 50,
+        'longtitle': true,
+        'theme': 'dark',
+        'onsuccess': onSuccess,
+        'onfailure': onFailure
+      });
+	  " . '
+    }
+  </script>
+	<form method="POST" action="">
+		<input id="i" type="text" name="idt" hidden>
+		<input id="s" type="submit" hidden>
+	</form>
+  <script src="https://apis.google.com/js/platform.js?onload=renderButton" async defer></script>';
+}
+$cid = $secret['id'];
+if (isset($_POST['idt'])) {
+	$id_token = $_POST['idt'];
+	$client = new Google_Client(['client_id' => $cid]);
+	$payload = $client->verifyIdToken($id_token);
+	if ($payload) {
+	  print_r($payload);
+	} else {
+	  http_response_code(400);
+	  echo "Failed to login!";
+	  showsignin();
+	}
+	echo "<br>";
+} else {
+	showsignin();
+}
