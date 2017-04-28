0b2a2639113a6a6f300c8419205fc28ba5458848
Kenton Vizdos <kvizdos@gmail.com>
2017-04-28 17:55:04 -0400

    Update header.php

--- forum/modules/header.php
+++ forum/modules/header.php
@@ -1,9 +1,12 @@
 <?php
+include '/home/ubuntu/keys/oauth.php';
+
 function showheader() {
      echo '
      <div id="header">
            <h1 id="title" ng-bind="title"></h1>
           <input type="text" placeholder="Search the forums!" class="rightnav" />
+          
           <small class="rightnav"><a href="login.php">Login / Register</a></small>
      </div>
 ';
