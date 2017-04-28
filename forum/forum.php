f940ea7b31063d4ac04ba5ca264679b0ef29ccf0
Kenton Vizdos <kvizdos@gmail.com>
2017-04-28 17:34:20 -0400

    Update forum.php

--- forum/forum.php
+++ forum/forum.php
@@ -7,6 +7,7 @@
     gcse.type = 'text/javascript';
     gcse.async = true;
     gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+    gcse.style.width = '100px';
     var s = document.getElementsByTagName('script')[0];
     s.parentNode.insertBefore(gcse, s);
   })();
