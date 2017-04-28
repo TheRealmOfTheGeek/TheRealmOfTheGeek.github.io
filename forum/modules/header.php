4bb7b176497d6a9baf6b1820e20bdf125dfd3bb5
Nick <nick-2017@users.noreply.github.com>
2017-04-28 22:40:25 +0100

    Update header.php

--- forum/modules/header.php
+++ forum/modules/header.php
@@ -1,6 +1,10 @@
-
+<?php
+function showheader() {
+     echo '
 <div id="header">
       <h1 id="title" ng-bind="title"></h1>
       <input type="text" placeholder="Search the forums!" class="rightnav" />
       <small class="rightnav"><a href="login.php">Login / Register</a></small>
-</div>
+</div>';
+}
+?>
