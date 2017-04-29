32e346cd029f5cde281cb92dd08846073ae62303
Kenton Vizdos <kvizdos@gmail.com>
2017-04-28 21:08:20 -0400

    Create setup-account.php

---
+++ forum/setup-account.php
@@ -0,0 +1,72 @@
+<html>
+  <head>
+    <title>Coding Forums</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
+  </head>
+  <body ng-app="" ng-init="title='Coding Forum'">
+    <div id="header">
+      <h1 id="title">{{ title }}</h1>
+      <small class="rightnav">Login</small>
+      <small class="rightnav">Register</small>
+    </div>
+
+    <style>
+    #su {
+      display: inline-block;
+      width: 78.5%;
+      background-color: #FDFDFD;
+      border: 5px solid #C7C7C7;
+      border-radius: 10px 0px 0px 10px;
+      margin-top: 20px;
+      margin-left: 20px;
+      height:485px;
+    }
+
+    #su .title {
+      background-color: white;
+      border: 3px solid #95989A;
+      height: 55px;
+      width: 97.5%;
+      margin: 10px;
+      color: #95989A;
+
+    }
+    #su input {
+      font-size: 37px;
+      width: 100%;
+      padding-left: 5px;
+    }
+    </style>
+
+    <div id="su">
+      <h2>Username</h2>
+      <div class="title">
+        <input type="textbox" placeholder="Username" id="username" name="username" required="yes" />
+      </div>
+
+      <h2>Email</h2>
+      <div class="title">
+        <input type="email" placeholder="Email" id="email" name="email" required="yes" />
+      </div>
+
+      <h2>Password</h2>
+      <div class="title">
+        <input type="password" placeholder="Password" id="password" name="password" required="yes" />
+      </div>
+
+      <h2>Repeat Password</h2>
+      <div class="title">
+        <input type="password" placeholder="Repeat Password" id="password" name="repassword" required="yes" />
+      </div>
+
+      <input type="submit" />
+
+    </div>
+
+    <div id="ad" class="right">
+      <p>adz</p>
+    </div>
+
+  </body>
+</html>
