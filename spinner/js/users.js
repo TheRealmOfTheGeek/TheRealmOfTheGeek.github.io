65e4b583e0e73e5641669ef247319155022e450a
Kento <kento@Kentos-Air.lan>
2017-05-11 22:42:26 -0400

    SPinner update

--- spinner/js/users.js
+++ spinner/js/users.js
@@ -1,67 +1,52 @@
-
 var users = [];
 
 $(document).ready(function() {
-
-  for (i = 0; i < users.length; i++) {
-     var table = document.getElementById("myTable");
-     var row = table.insertRow(0);
-     var user = row.insertCell(0);
-
-     user.innerHTML = users[i] + "";
-
-  }
-
-  var table = document.getElementById("myTable"),
+  var table = document.getElementById("usrTable"),
       rows = table.getElementsByTagName("tr"),
       i;
 
   function onRowClick(tableId, callback) {
-    for (i = 0; i < rows.length; i++) {
-        table.rows[i].onclick = function (row) {
-            return function () {
-                callback(row);
-            };
-        }(table.rows[i]);
+    for(i = 0; i < rows.length; i++) {
+      table.rows[i].onclick = function(row) {
+        return function() {
+          callback(row);
+        };
+      } (table.rows[i]);
     }
-  };
+  }
 
-  $('table').on('click', 'tr', function(e){
-    var index = users.indexOf($(this).closest('tr').value);
-      users.splice(index, 1)
+  $('table').on('click', 'tr', function(e) {
+    var index = users.indexOf($(this).closest('tr').value)
 
-     $(this).closest('tr').remove()
-     recreateWheel();
+    users.splice(index, 1);
 
-  })
+    $(this).closest('tr').remove();
+
+    // TODO: ADD SPIN STUFF
+  });
+
+
+
+
+
+
 
 });
-
-function deleteRow(r) {
-    var i = r.parentNode.parentNode.rowIndex;
-
-    document.getElementById("myTable").deleteRow(i);
-}
 
 function addUser() {
   var usr = document.getElementById("userInput").value;
   var form = document.getElementById("form");
 
-
-
-
   users.push(usr+"");
 
-  var table = document.getElementById("myTable");
-      var row = table.insertRow(0);
-      var cell1 = row.insertCell(0);
+  var table = document.getElementById("usrTable"),
+      row = table.insertRow(0);
+      cell1 = row.insertCell(0);
       cell1.innerHTML = usr;
 
-      form.reset();
+  form.reset();
 
-      recreateWheel();
-
-      for (i = 0; i < users.length; i++) {
-        console.log(users[i]);
-      }
+  for(i = 0; i < users.length; i++) {
+    console.log(users[i]);
+  }
 }
