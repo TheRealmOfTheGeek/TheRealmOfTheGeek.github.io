7604e33fc56f3557a17a48a3ef4406397ddbad5f
Kento <kento@Kentos-Air.lan>
2017-05-11 22:48:04 -0400

    spinner update

--- spinner/js/users.js
+++ spinner/js/users.js
@@ -1,52 +1,65 @@
 var users = [];
 
+
+
+
 $(document).ready(function() {
+
   var table = document.getElementById("usrTable"),
       rows = table.getElementsByTagName("tr"),
       i;
 
   function onRowClick(tableId, callback) {
-    for(i = 0; i < rows.length; i++) {
-      table.rows[i].onclick = function(row) {
-        return function() {
-          callback(row);
-        };
-      } (table.rows[i]);
+    for (i = 0; i < rows.length; i++) {
+        table.rows[i].onclick = function (row) {
+            return function () {
+                callback(row);
+            };
+        }(table.rows[i]);
     }
-  }
-
-  $('table').on('click', 'tr', function(e) {
-    var index = users.indexOf($(this).closest('tr').value)
-
-    users.splice(index, 1);
-
-    $(this).closest('tr').remove();
-
-    // TODO: ADD SPIN STUFF
-  });
+  };
 
 
 
 
 
 
+  $('table').on('click', 'tr', function(e){
+    var index = users.indexOf($(this).closest('tr').value);
+      users.splice(index, 1)
+
+     $(this).closest('tr').remove()
+     recreateWheel();
+
+  })
 
 });
+
+function deleteRow(r) {
+    var i = r.parentNode.parentNode.rowIndex;
+
+    document.getElementById("usrTable").deleteRow(i);
+}
 
 function addUser() {
   var usr = document.getElementById("userInput").value;
   var form = document.getElementById("form");
 
+
+
+
   users.push(usr+"");
 
-  var table = document.getElementById("usrTable"),
-      row = table.insertRow(0);
-      cell1 = row.insertCell(0);
+  var table = document.getElementById("usrTable");
+      var row = table.insertRow(0);
+      var cell1 = row.insertCell(0);
       cell1.innerHTML = usr;
 
-  form.reset();
+      form.reset();
 
-  for(i = 0; i < users.length; i++) {
-    console.log(users[i]);
-  }
+      recreateWheel();
+
+      for (i = 0; i < users.length; i++) {
+        console.log(users[i]);
+      }
 }
