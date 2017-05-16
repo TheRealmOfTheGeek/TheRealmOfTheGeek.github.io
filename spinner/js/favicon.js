b5d5e1610f74a08a12fff4d85daecfd7ccc4bd19
Kenton Vizdos <kvizdos@gmail.com>
2017-05-16 16:14:46 -0400

    Fix 2

--- spinner/js/favicon.js
+++ spinner/js/favicon.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  if(window.location == "therotg.com/spinner/" || window.location == "therotg.com/spinner/index.html") {
+  if(window.location == "https://therotg.com/spinner/" || window.location == "https://therotg.com/spinner/index.html") {
     $("#favicon").attr("href","logo.png");
   } else {
     $("#favicon").attr("href","dev.png");
