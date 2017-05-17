4460fcb2ce5a791c8b24a5300898d9eb75f19fc3
Kenton Vizdos <kvizdos@gmail.com>
2017-05-17 15:54:03 -0400

    navbar

--- modules/navbar.php
+++ modules/navbar.php
@@ -1,20 +1,11 @@
-<nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header navbar">
-
-      <a class="navbar-brand" href="../home.php">TheROTG.com</a>
-    </div>
-    <div class="collapse navbar-collapse" id="myNavbar" data-spy="affix" data-offset-top="645">
-      <ul class="nav navbar-nav">
-        <li><a href="../home.php#about">About</a></li>
-        <li><a href="../home.php#plugins">Projects</a></li>
-        <li><a href="../home.php#gig">Hire Me</a></li>
-        <li><a href="../home.php#gig">Contact</a></li>
-        <li><a href="../calendar">Calendar</a></li>
-        <li><a href="../videos">Learn To Code</a></li>
-        <li><a href="../games">Fun Games</a></li>
-
-      </ul>
-    </div>
-  </div>
+<nav class="navbar navbar-inverse" data-spy="affix" data-offset-top="645">
+  <ul class="nav navbar-nav">
+    <li><a href="../home.php#about">About</a></li>
+    <li><a href="../home.php#plugins">Projects</a></li>
+    <li><a href="../home.php#gig">Hire Me</a></li>
+    <li><a href="../home.php#gig">Contact</a></li>
+    <li><a href="../calendar">Calendar</a></li>
+    <li><a href="../videos">Learn To Code</a></li>
+    <li><a href="../games">Fun Games</a></li>
+  </ul>
 </nav>
