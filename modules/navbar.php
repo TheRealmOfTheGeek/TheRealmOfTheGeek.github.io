4772f704485ebb75da95566d511ba583df6f4114
Kenton Vizdos <kvizdos@gmail.com>
2017-05-17 16:14:39 -0400

    navbar

--- modules/navbar.php
+++ modules/navbar.php
@@ -19,8 +19,8 @@
 }
 </style>
 <div class="nav-wrapper">
-<nav class="navbar navbar-inverse navbar-collapse" data-spy="affix" data-offset-top="645">
-  <ul class="nav navbar-nav">
+<nav class="navbar navbar-inverse" data-spy="affix" data-offset-top="645">
+  <ul class="nav navbar-nav collapse navbar-collapse">
     <li><a href="../home.php#about">About</a></li>
     <li><a href="../home.php#plugins">Projects</a></li>
     <li><a href="../home.php#gig">Hire Me</a></li>
