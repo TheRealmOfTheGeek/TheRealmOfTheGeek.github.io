7b35c77cacac18609ca2d44a56544f7468ad919f
Kenton Vizdos <kvizdos@gmail.com>
2017-05-17 15:37:52 -0400

    js

--- modules/navbar.php
+++ modules/navbar.php
@@ -1,5 +1,5 @@
 <script src="../static/navbar.js"></script>
-<nav class="navbar navbar-inverse" data-spy="affix" data-offset-top="192">
+<nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <div class="navbar-header navbar">
 
--- static/navbar.js
+++ static/navbar.js
@@ -1,29 +1,20 @@
-function Utils() {
+function isScrolledIntoView(elem)
+{
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
 
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
 }
 
-Utils.prototype = {
-    constructor: Utils,
-    isElementInView: function (element, fullyInView) {
-        var pageTop = $(window).scrollTop();
-        var pageBottom = pageTop + $(window).height();
-        var elementTop = $(element).offset().top;
-        var elementBottom = elementTop + $(element).height();
+document.getElementsByTagName("body").addEventListener("scroll", scrollFunc);
 
-        if (fullyInView === true) {
-            return ((pageTop < elementTop) && (pageBottom > elementBottom));
-        } else {
-            return ((elementTop <= pageBottom) && (elementBottom >= pageTop));
-        }
-    }
-};
-
-var Utils = new Utils();
-
-var isElementInView = Utils.isElementInView($('#header'), false);
-
-if (isElementInView) {
-    console.log('in view');
-} else {
-    console.log('out of view');
+function scrollFunc() {
+  if(isScrolledIntoView("#header")) {
+    console.log("in view");
+  } else {
+    console.log("out of view");
+  }
 }
