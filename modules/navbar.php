15fe407170afceed926ccad9af89d7f2408d7758
Kenton Vizdos <kvizdos@gmail.com>
2017-05-17 16:16:25 -0400

    nav

--- modules/navbar.php
+++ modules/navbar.php
@@ -18,9 +18,9 @@
     min-height:50px;
 }
 </style>
-<div class="nav-wrapper">
+<div class="nav-wrapper collapse navbar-collapse">
 <nav class="navbar navbar-inverse" data-spy="affix" data-offset-top="645">
-  <ul class="nav navbar-nav collapse navbar-collapse">
+  <ul class="nav navbar-nav">
     <li><a href="../home.php#about">About</a></li>
     <li><a href="../home.php#plugins">Projects</a></li>
     <li><a href="../home.php#gig">Hire Me</a></li>
