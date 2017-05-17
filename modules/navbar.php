321daf073c65e6986e1cc02d0bf94e9046992302
Kenton Vizdos <kvizdos@gmail.com>
2017-05-17 16:03:03 -0400

    fix hopefully

--- modules/navbar.php
+++ modules/navbar.php
@@ -8,8 +8,13 @@
     width: 100%;
     z-index:10;
 }
+.nav-wrapper
+{
+    min-height:50px;
+}
 </style>
-<nav class="navbar navbar-inverse" data-spy="affix" data-offset-top="600">
+<div class="nav-wrapper">
+<nav class="navbar navbar-inverse" data-spy="affix" data-offset-top="645">
   <ul class="nav navbar-nav">
     <li><a href="../home.php#about">About</a></li>
     <li><a href="../home.php#plugins">Projects</a></li>
@@ -20,3 +25,4 @@
     <li><a href="../games">Fun Games</a></li>
   </ul>
 </nav>
+</div>
