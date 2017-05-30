c5c1c4aa19b7790a98236714ed5ae7fbae888e50
Kenton Vizdos <kvizdos@gmail.com>
2017-05-30 14:59:07 -0400

    Ads

--- modules/header.php
+++ modules/header.php
@@ -1,12 +1,12 @@
 
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-	
-<title>TheROTG.com</title>  
+
+<title>TheROTG.com</title>
 
   <link rel="stylesheet" href="../static/bootstrap.css">
   <link rel="stylesheet" href="../style.css">
- 
+
   <script src="../static/jquery.js"></script>
   <script src="../static/rotg.js"></script>
   <script src="../static/bootstrap.js"></script>
@@ -24,19 +24,21 @@
   }
   </style>
     <script>
-  (function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,"script","https:"//www.google-analytics.com/analytics.js","ga");
-  ga("create", "UA-47286500-3", "auto");
-  ga("send", "pageview");
-  </script>
-    
-  <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-  <script>
-    (adsbygoogle = window.adsbygoogle || []).push({
-      google_ad_client: "ca-pub-4499070912220787",
-      enable_page_level_ads: true
-    });
+        (function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,"script","https:"//www.google-analytics.com/analytics.js","ga");
+        ga("create", "UA-47286500-3", "auto");
+        ga("send", "pageview");
   </script>
 
+  <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+  <!-- ROTGMain -->
+  <ins class="adsbygoogle"
+       style="display:block"
+       data-ad-client="ca-pub-4499070912220787"
+       data-ad-slot="6092543559"
+       data-ad-format="auto"></ins>
+  <script>
+  (adsbygoogle = window.adsbygoogle || []).push({});
+  </script>
