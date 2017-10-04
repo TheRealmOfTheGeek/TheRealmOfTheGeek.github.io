0b181630e1bb1f8219f0164006ec104e333461c5
Kenton Vizdos <kvizdos@gmail.com>
2017-10-04 16:58:56 -0400

    Update home.php

--- home.php
+++ home.php
@@ -1,8 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
 	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
 	<script src="https://apis.google.com/js/platform.js"></script>
 
