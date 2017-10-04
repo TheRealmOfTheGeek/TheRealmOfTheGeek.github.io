15397565479821978ea787485c020e62bc937341
Kenton Vizdos <kvizdos@gmail.com>
2017-10-03 21:26:40 -0400

    Fixes #20 #21

--- modules/header.php
+++ modules/header.php
@@ -23,20 +23,4 @@
     height: 100%:
   }
   </style>
-    <script>
-        (function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,"script","https:"//www.google-analytics.com/analytics.js","ga");
-        ga("create", "UA-47286500-3", "auto");
-        ga("send", "pageview");
-  </script>
 
-  <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-  <!-- ROTGMain -->
-  <ins class="adsbygoogle"
-       style="display:block"
-       data-ad-client="ca-pub-4499070912220787"
-       data-ad-slot="6092543559"
-       data-ad-format="auto"></ins>
-  
