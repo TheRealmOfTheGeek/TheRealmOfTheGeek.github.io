7bd657ec7829dfd28d96728e0f4ce22782cc6128
Ryan Hammond <ryanhammond13@gmail.com>
2017-10-03 21:16:05 -0500

    Revert "Updated navbar click function"
    This reverts commit 29f5ea5e7a3ed2e7bd7da1a21b7ffb0b75b3c289.

--- static/nav.js
+++ static/nav.js
@@ -5,11 +5,11 @@ $(document).ready(function(){
         event.preventDefault();
         var hash = this.hash;
 
-        // Push the hash to the URL without forcing page refresh
-        history.pushState(null, null, hash);
-
         $('html, body').animate({
           scrollTop: $(hash).offset().top - $(".navbar").height()
+        }, 800, function(){
+
+          window.location.hash = hash;
         });
     }
   });
